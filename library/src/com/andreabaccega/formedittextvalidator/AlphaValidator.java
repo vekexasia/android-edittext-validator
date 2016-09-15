c35bf11bab8a94f63302d5c38d24652ba3e84be8
Andrea Baccega <vekexasia@gmail.com>
2016-09-15 13:34:24 +0200

    Added unicode support

--- library/src/com/andreabaccega/formedittextvalidator/AlphaValidator.java
+++ library/src/com/andreabaccega/formedittextvalidator/AlphaValidator.java
@@ -2,6 +2,6 @@ package com.andreabaccega.formedittextvalidator;
 
 public class AlphaValidator extends RegexpValidator {
 	public AlphaValidator(String message) {
-		super(message, "[A-z\u00C0-\u00ff \\./-]*");
+		super(message, "[A-z\u00C0-\u00ff \\./-\\?]*");
 	}
 }
